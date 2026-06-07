2f2bce092bc7d7b4b77ba6f19d391481868090a6
Ratmir <ratmirkach2000@gmail.com>
2026-06-07 09:57:05 +0600

    фикс: config.js обратно в git (на Vercel нет секретов)

---
+++ config.js
@@ -0,0 +1,11 @@
+const CONFIG = {
+  CITY: 'Bishkek',
+  CITY_LAT: 42.87,
+  CITY_LON: 74.59,
+  UPDATE_INTERVAL: 60,
+
+  EXCHANGE_BASE: 'USD',
+  EXCHANGE_TARGETS: ['USD', 'EUR', 'RUB'],
+
+  FACT_API: 'https://uselessfacts.jsph.pl/api/v2/facts/random?language=en',
+};
